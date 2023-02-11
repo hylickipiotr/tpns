2c9dfab0ffcca20cf3776b34d486b9bfa0479ce1
Piotr Hylicki <hylickipiotr@gmail.com>
2023-02-11 18:11:22 +0100

    Removed checking req method

--- pages/api/olx/search.ts
+++ pages/api/olx/search.ts
@@ -145,11 +145,6 @@ export default async function search(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "GET") {
-    res.status(405).json({ message: "Method not allowed" });
-    return;
-  }
-
   try {
     QuerySchema.parse(req.query);
   } catch (error) {
